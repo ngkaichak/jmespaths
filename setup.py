1dd3d209ae901d430df84a878719c975297c8982
ngkaichak <walkzombie@163.com>
2023-01-30 16:10:57 +0800

    Version: 1.0.1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     #name='jmespath',
     name='jmespaths',
-    version='1.0.1.5',
+    version='1.0.1.6',
     description='JSON Matching Expressions',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     author='James Saryerwinnie',
